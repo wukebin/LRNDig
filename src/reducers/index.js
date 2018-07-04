cb6eb0a645b7a2b1ff7b2c487b6ab8ee55857d2c
Axel <falkaxel96@gmail.com>
2018-07-04 10:13:41 +0200

    updated 4/7

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import ProvReducer from './ProvReducer';
+import ElevReducer from './ElevReducer';
 export default combineReducers ( {
-    prov: ProvReducer
-
+    prov: ProvReducer,
+    elev: ElevReducer
 }); 
