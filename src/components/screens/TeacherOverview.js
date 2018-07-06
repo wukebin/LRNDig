12c51ee376eb2de751b7f43b40d0d921a02dc6c9
wukebin <dic14kng@student.lu.se>
2018-07-06 11:33:13 +0200

    added spinner during load time

--- src/components/screens/TeacherOverview.js
+++ src/components/screens/TeacherOverview.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, ListView, TouchableHighlight } from 'react-native';
-import { Header, Button, Card, CardSection } from '../common';
+import { Header, Button, Card, CardSection, Spinner } from '../common';
 import firebase from 'firebase';
 
 var config = {
@@ -17,10 +17,10 @@ export default class TeacherOverview extends Component {
     constructor() {
         super();
         this.path = firebase.database().ref('users');
-        super();
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
             userDataSource: ds,
+            loading: true
         };
 
     }
@@ -39,7 +39,7 @@ export default class TeacherOverview extends Component {
                 })
             })
 
-            this.setState({ userDataSource: this.state.userDataSource.cloneWithRows(students) })
+            this.setState({ userDataSource: this.state.userDataSource.cloneWithRows(students), loading: false })
         })
     }
 
@@ -57,13 +57,21 @@ export default class TeacherOverview extends Component {
             </TouchableHighlight>
         );
     }
-    render() {
-        return (
-            <ListView
+
+    renderContent() {
+        if (this.state.loading) {
+            return ( <Spinner size="large"/>)
+        }
+        return(
+             <ListView
                 dataSource={this.state.userDataSource}
                 renderRow={this.renderRow.bind(this)}
             />
-
+        )
+    }
+    render() {
+        return (
+           this.renderContent()
         );
     }
 }
