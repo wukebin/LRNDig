ed85cd05b98055a9f185a8807692882a1c93891b
wukebin <dic14kng@student.lu.se>
2018-07-25 16:53:52 +0200

    backgroundimage

--- src/components/screens/TeacherView.js
+++ src/components/screens/TeacherView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { Header, Button, Card, Divider } from 'react-native-elements';
 
 
@@ -14,10 +14,17 @@ export default class TeacherView extends Component {
     render() {
         return (
             <View style ={{flex:1}}>
-             <Header
+            <ImageBackground
+                source= {require('../images/bg.jpg')}
+                style={{width: '100%', height: '100%'}}
+                >
+                 <Header
                     leftComponent={{ icon: 'menu', color: '#fff' }}
                     centerComponent={{ text: this.Capitalize(this.state.name), style: { color: '#fff', fontSize: 24 } }}
                     rightComponent={{ icon: 'home', color: '#fff' }}
+                    outerContainerStyles={{borderBottomWidth:0, elevation:2}}
+                    backgroundColor= '#2f7399'
+                
                 />
             
             
@@ -26,7 +33,7 @@ export default class TeacherView extends Component {
                 <Button
                     onPress={() => this.props.navigation.navigate('CreateTestView', {name: this.state.name})}
                     title='Skapa Prov'
-                    containerViewStyle = {{marginBottom: 5, marginTop: 5}}
+                    containerViewStyle = {{marginBottom: 5}}
                     raised
                     large
                     backgroundColor='#2f7399'
@@ -59,6 +66,7 @@ export default class TeacherView extends Component {
                     borderRadius={10}
                 />
             </View>
+            </ImageBackground>
             </View>
         );
     }
