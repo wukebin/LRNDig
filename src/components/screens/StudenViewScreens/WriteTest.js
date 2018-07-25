6a81c3b6939b51bb4ec3a847ada2ef9f010e2a2c
Axel <falkaxel96@gmail.com>
2018-07-25 10:53:07 +0200

    Created WriteTest

---
+++ src/components/screens/StudenViewScreens/WriteTest.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import {FlatList} from 'react-native';
+import {connect } from 'react-redux';
+import firebase from 'firebase';
+
+
+class WriteTest extends Component {
